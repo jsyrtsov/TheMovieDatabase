cdb02e205ed608b139df1140497a987f1ac98e95
jsyrtsov <jsyrtsov@gmail.com>
2020-02-21 18:50:39 +0300

    Initial commit

---
+++ TheMovieDatabase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieDatabase
+//
+//  Created by Evgeny Syrtsov on 2/21/20.
+//  Copyright © 2020 Evgeny Syrtsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
