61b801f220036e0f53a41eb4809dec79d139a5b7
jsyrtsov <jsyrtsov@gmail.com>
2020-02-22 00:13:41 +0300

    added tableView and few VCs

---
+++ TheMovieDatabase/Feed/FeedViewController.swift
@@ -0,0 +1,53 @@
+//
+//  FeedViewController.swift
+//  TheMovieDatabase
+//
+//  Created by Evgeny Syrtsov on 2/21/20.
+//  Copyright © 2020 Evgeny Syrtsov. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+extension FeedViewController: UITableViewDelegate {
+    
+}
+extension FeedViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath) as? MovieTableViewCell
+        return cell ?? UITableViewCell()
+    }
+    
+    
+}
---
+++ TheMovieDatabase/Feed/MovieTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieTableViewCell.swift
+//  TheMovieDatabase
+//
+//  Created by Evgeny Syrtsov on 2/21/20.
+//  Copyright © 2020 Evgeny Syrtsov. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var movieNameLabel: UILabel!
+    @IBOutlet weak var movieDescriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
