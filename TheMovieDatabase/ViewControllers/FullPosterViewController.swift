513b742d3939826f00d7e2fe7f5da02ce9a6adfa
jsyrtsov <jsyrtsov@gmail.com>
2020-03-05 08:28:32 +0300

    almost done

---
+++ TheMovieDatabase/ViewControllers/FullPosterViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FullPosterViewController.swift
+//  TheMovieDatabase
+//
+//  Created by Evgeny Syrtsov on 3/4/20.
+//  Copyright © 2020 Evgeny Syrtsov. All rights reserved.
+//
+
+import UIKit
+
+class FullPosterViewController: UIViewController {
+
+    var movieId: Int?
+    var posterPath: String?
+
+    @IBOutlet weak private var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak private var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+        loadPic()
+    }
+
+    private func configureView() {
+        activityIndicator.startAnimating()
+    }
+
+    private func loadPic() {
+        imageView.loadFullPic(withPath: posterPath)
+    }
+}
